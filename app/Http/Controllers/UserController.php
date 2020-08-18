b7bd32fb1cd8475a81558ac5f907c7beb4ff0b98
Javier Amaya <Javieramayapat@gmail.com>
2020-08-18 00:20:27 -0500

    Added functions CRUD of User

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,20 +2,42 @@
 
 namespace App\Http\Controllers;
 
+use App\User;
+// Clase que representa una solicitud http
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
+    // Show user information 
     public function index()
     {
-        // Consultar los datos
+        // lok for all user data into database
+        $users = User::orderBy('id')->get();
+        
+        // view recive two params ('name of view, [$data])
+        return view('users.index',[
+            'users' => $users
+        ]);
+    }
+    
+    // Save user information into database
+    public function store(Request $request)
+    {
+        User::create([ 
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => $request->password
+        ]);
 
-        // Cargo la vista y paso los datos
+        // back() allowed me to return information to the last route
+        return back();
     }
-    public function store()
+
+    // delete user information selected
+    public function destroy(User $user)
     {
-    }
-    public function destroy()
-    {
+        $user->delete();
+
+        return back();
     }
 }
